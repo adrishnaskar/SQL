b26bbf5ad9000e4d8f93f59fc1a485f4e26e22f6
Adrish Naskar <hiadrish@gmail.com>
2021-11-08 17:09:16 +0530

    Updated the Schema

---
+++ Online Phone Shop/data.sql
@@ -0,0 +1,9 @@
+CREATE TABLE phone_details 
+(name VARCHAR(100), 
+ description VARCHAR(255), 
+ availability CHAR, 
+ price FLOAT, 
+ in_stock INT, 
+ rating FLOAT);
+ 
+ 
---
+++ Online Phone Shop/schema.sql
@@ -0,0 +1,4 @@
+INSERT INTO phone_details VALUES ('Samsung 7x', 'phone with 8mp camera & voice recording', 'y', 3999.0, 3, 4.5);
+INSERT INTO phone_details VALUES ('sony 98x', 'phone with 32GB memory & Bluetooth', 'y', 6999.0, 5, 4);
+INSERT INTO phone_details VALUES ('realme 3', 'phone with 8mp camera & wifi calling', 'y', 4999.0, 10, 3.5);
+INSERT INTO phone_details VALUES ('one plus 4', 'phone with 64GB memory, wifi, Bluetooth, 16mp camera', 'y', 9999.0, 7, 4.5);
