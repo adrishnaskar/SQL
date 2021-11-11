2d7aae544fe56e08e4519843d224100dee25d8e2
Adrish Naskar <hiadrish@gmail.com>
2021-11-11 11:02:20 +0530

    Daily Grocery List Keeping

---
+++ Daily Grocery List/data.sql
@@ -0,0 +1,46 @@
+INSERT INTO grocery_list (item_no,item_name,price,is_available,needed,quantity)VALUES (
+  '1',
+  'Rice',
+  '150',
+  'TRUE',
+  'FALSE',
+  '2000'
+);
+
+INSERT INTO grocery_list (item_no,item_name,price,is_available,needed,quantity)VALUES (
+  '2',
+  'Flour',
+  '245',
+  'FALSE',
+  'TRUE',
+  '5000'
+);
+
+INSERT INTO grocery_list (item_no,item_name,price,is_available,needed,quantity) VALUES (
+  '3',
+  'Dal',
+  '91',
+  'TRUE',
+  'FALSE',
+  '500'
+);
+
+INSERT INTO grocery_list (item_no,item_name,price,is_available,needed,quantity) VALUES (
+  '4',
+  'Coffee',
+  '550',
+  'TRUE',
+  'FALSE',
+  '200'
+);
+
+INSERT INTO grocery_list (item_no,item_name,price,is_available,needed,quantity) VALUES (
+  '5',
+  'Spice',
+  '64',
+  'FALSE',
+  'TRUE',
+  '200'
+);
+
+
---
+++ Daily Grocery List/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE grocery_list
+(item_no INT,
+item_name VARCHAR(30),
+price INT,
+is_available BOOLEAN,
+needed BOOLEAN,
+quantity INT,
+
+PRIMARY KEY (item_no)
+);
